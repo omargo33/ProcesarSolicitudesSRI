6b3e810ab5433460696e7a2a681f92c8902f82db
Omar Velez <omargo33@gmail.com>
2024-05-20 00:23:57 -0500

    Update Test.java

--- procesamiento/src/main/java/com/leon/batch/Test.java
+++ procesamiento/src/main/java/com/leon/batch/Test.java
@@ -5,24 +5,178 @@ import com.aplicaciones13.documentos.estructuras.factura.v2_1_0.Factura.Detalles
 import com.aplicaciones13.documentos.estructuras.factura.v2_1_0.Factura;
 import com.aplicaciones13.documentos.utilidades.Conversion;
 import com.aplicaciones13.documentos.utilidades.Route;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Test {
 
-    public static void main(String[] args) {
+    double baseImponibleIva0 = 0;
+    double baseImponibleIva = 0;
+    double valorIva = 0;
+    double baseImponibleIvaVigente = 0;
+    double valorIvaVigente = 0;
+    double baseImponibleICE = 0;
+    double valorICE = 0;
+    double baseImponibleIRBPNR = 0;
+    double valorIRBPNR = 0;
+    String mensajeInfoAdicional = "";
+    double montoTotalConImpuestos = 0;
 
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.procesar();
+
+    }
+
+    public void procesar() {
         Autorizacion autorizacion = Route.getAuthorization(
                 "/home/ovelez/Documentos/clientes/Leon/documentos/0103153425001/1791984722001/01/2024-04-02/27210020000853005932341.xml");
-
-        log.info(autorizacion.getComprobante());
         Factura factura = Conversion.xmlToPojo(autorizacion.getComprobante(), Factura.class);
-        log.info(factura.getInfoTributaria().getRuc());
 
         Detalles detalles = factura.getDetalles();
-
         detalles.getDetalle().forEach(detalle -> {
-            log.info(detalle.getDescripcion());
+            log.info("factura: {} {} {} {} {} {} {} {} {} {} {} {}",
+                    factura.getInfoTributaria().getRazonSocial(),
+                    factura.getInfoTributaria().getRuc(),
+                    factura.getInfoTributaria().getClaveAcceso(),
+                    factura.getInfoTributaria().getCodDoc(),
+                    factura.getInfoTributaria().getEstab(),
+                    factura.getInfoTributaria().getPtoEmi(),
+                    factura.getInfoTributaria().getSecuencial(),
+                    factura.getInfoTributaria().getDirMatriz(),
+                    factura.getInfoTributaria().getAgenteRetencion(),
+                    factura.getInfoTributaria().getContribuyenteRimpe(),
+                    factura.getInfoTributaria().getTipoEmision(),
+                    factura.getInfoTributaria().getAmbiente());
+
+            log.info("infoFactura {} {} {} {} {} {} {} {} {} {} ", factura.getInfoFactura().getFechaEmision(),
+                    factura.getInfoFactura().getDirEstablecimiento(),
+                    factura.getInfoFactura().getContribuyenteEspecial(),
+                    factura.getInfoFactura().getObligadoContabilidad(),
+                    factura.getInfoFactura().getTipoIdentificacionComprador(),
+                    factura.getInfoFactura().getRazonSocialComprador(),
+                    factura.getInfoFactura().getIdentificacionComprador(),
+                    factura.getInfoFactura().getDireccionComprador()
+            // , factura.getInfoFactura().getRise()
+                    , factura.getInfoFactura().getMoneda(), factura.getInfoFactura().getPropina());
+
+            montoTotalConImpuestos += factura.getInfoFactura().getPropina().doubleValue();
+
+            log.info("detalle: {} {} {} {} {} {} {} {} {}", String.valueOf(detalle.getCantidad()),
+                    detalle.getCodigoAuxiliar(),
+                    detalle.getCodigoPrincipal(), detalle.getDescripcion(), detalle.getDescuento().toString(),
+                    String.valueOf(detalle.getPrecioSinSubsidio()), String.valueOf(detalle.getPrecioTotalSinImpuesto()),
+                    String.valueOf(detalle.getPrecioUnitario()), detalle.getUnidadMedida());
+
+            detalle.getImpuestos().getImpuesto().forEach(impuesto -> {
+
+                //log.info("impuesto: {} {} {} {} {}", impuesto.getBaseImponible(), String.valueOf(impuesto.getValor()),
+                //        impuesto.getCodigo(), impuesto.getCodigoPorcentaje(), String.valueOf(impuesto.getTarifa()));
+
+                if (impuesto.getCodigo().equals("2")) {
+                    baseImponibleIva += impuesto.getBaseImponible().doubleValue();
+                    valorIva += impuesto.getValor().doubleValue();
+
+                    if (impuesto.getCodigoPorcentaje().compareTo("0") == 0) {
+                        baseImponibleIva0 += impuesto.getBaseImponible().doubleValue();
+                    } else {
+                        baseImponibleIvaVigente += impuesto.getBaseImponible().doubleValue();
+                        valorIvaVigente += impuesto.getValor().doubleValue();
+                        log.info("IVA: {} {} {}", impuesto.getBaseImponible(), impuesto.getValor(),
+                                porcentajeIva(impuesto.getCodigoPorcentaje()));
+                    }
+                    montoTotalConImpuestos += impuesto.getBaseImponible().doubleValue()
+                            + impuesto.getValor().doubleValue();
+
+                }
+                if (impuesto.getCodigo().equals("3")) {
+                    baseImponibleICE += impuesto.getBaseImponible().doubleValue();
+                    valorICE += impuesto.getValor().doubleValue();
+                }
+                if (impuesto.getCodigo().equals("5")) {
+                    baseImponibleIRBPNR += impuesto.getBaseImponible().doubleValue();
+                    valorIRBPNR += impuesto.getValor().doubleValue();
+                }
+            });
+
+            log.info("impuesto: {} {} {} {} {} {} {} {} {} {} {}",
+                    baseImponibleIva0,
+                    baseImponibleIva,
+                    valorIva,
+                    baseImponibleIvaVigente,
+                    valorIvaVigente,
+                    baseImponibleICE,
+                    valorICE,
+                    baseImponibleIRBPNR,
+                    valorIRBPNR,
+                    mensajeInfoAdicional,
+                    montoTotalConImpuestos);
+
+            try {
+                detalle.getDetallesAdicionales().getDetAdicional().forEach(detalleAdicional -> {
+                    log.info("detalle adicional: {} {}", detalleAdicional.getNombre(),
+                            String.valueOf(detalleAdicional.getValor()));
+                });
+
+            } catch (NullPointerException e) {
+                log.info("No hay detalles adicionales");
+            }
+
+            try {
+                factura.getInfoAdicional().getCampoAdicional().forEach(campoAdicional -> {
+                    String nombre = campoAdicional.getNombre();
+                    String valor = campoAdicional.getValue();
+                    mensajeInfoAdicional = mensajeInfoAdicional + String.format("%s: %s;", nombre, valor);
+
+                    if (nombre != null && nombre.toUpperCase().startsWith("DIRECCI")) {
+                        log.info("campo adicional direccion: {} {}", nombre, valor);
+                    }
+                    if (nombre != null && nombre.toUpperCase().startsWith("OBERVACI")) {
+                        log.info("campo adicional observacion: {} {}", nombre, valor);
+                    }
+                });
+
+                if (mensajeInfoAdicional != null && !mensajeInfoAdicional.isEmpty()) {
+                    log.info("mensajeInfoAdicional: {}", mensajeInfoAdicional);
+                }
+            } catch (Exception e) {
+                log.error("No hay campos adicionales", e.toString());
+            }
         });
+    }
+
+    /**
+     * Metodo que retorna el porcentaje de IVA segun el codigo de porcentaje
+     * 
+     * @param codigo
+     * @return
+     */
+    private double porcentajeIva(String codigo) {
+
+        switch (codigo) {
+            case "0":
+                return 0;
+
+            case "2":
+                return 12;
+
+            case "3":
+                return 14;
+
+            case "4":
+                return 15;
+
+            case "5":
+                return 5;
+
+            case "10":
+                return 13;
+
+            default:
+                log.warn("Codigo de porcentaje de IVA no encontrado: {}", codigo);
+                return -1;
+
+        }
     }
 }
